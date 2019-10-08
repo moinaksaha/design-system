e5cda2c1050ac67efb957a8a26339f15884a8852
Dilip Kr. Shukla <dilip.shukla@comeon.com>
2019-10-08 15:41:28 +0200

    Test pika cd (#16)
    * add module information
    
    * use comeon-stockholm to match organisation scope from github
    
    * remove styled-component from dependency

---
+++ mod.mjs
@@ -0,0 +1,13 @@
+export * as blitzino from './dist/blitzino/constants.mjs';
+export * as casinostugan from './dist/casinostugan/constants.mjs';
+export * as comeon from './dist/comeon/constants.mjs';
+export * as folkeautomaten from './dist/folkeautomaten/constants.mjs';
+export * as galaksino from './dist/galaksino/constants.mjs';
+export * as getlucky from './dist/getlucky/constants.mjs';
+export * as hajper from './dist/hajper/constants.mjs';
+export * as mobilautomaten from './dist/mobilautomaten/constants.mjs';
+export * as mobilebet from './dist/mobilebet/constants.mjs';
+export * as pzbuk from './dist/pzbuk/constants.mjs';
+export * as snabbare from './dist/snabbare/constants.mjs';
+export * as suomikasino from './dist/suomikasino/constants.mjs';
+
